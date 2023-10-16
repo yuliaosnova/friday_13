a0b66148600f644fe770cf0b5fb50f4769fae9c4
Yulia Osnova <yulia.osnova@gmail.com>
2023-10-16 09:53:04 +0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.enableCors();
   await app.listen(3001);
