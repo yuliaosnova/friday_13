a18fd8eba2d7a769c205348ad02721667bf1e05d
Yulia Osnova <yulia.osnova@gmail.com>
2023-10-16 10:26:06 +0300

    add method in versel.js

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({
-    credentials: true,
-    origin: 'http://localhost:5173',
-  });
+  //   app.enableCors({
+  //     credentials: true,
+  //     origin: 'http://localhost:5173',
+  //   });
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
