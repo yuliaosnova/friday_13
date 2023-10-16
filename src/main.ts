20003f7d656ce5cc4bc5ce0aba29e95c806ba486
Yulia Osnova <yulia.osnova@gmail.com>
2023-10-16 10:02:31 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
